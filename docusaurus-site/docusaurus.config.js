b66e5357f951e36e543cc018d6c350e10855bf94
jemimah2 <jemimah@elixxir.io>
2021-12-16 18:46:18 +0100

    Remove Blog link

--- docusaurus-site/docusaurus.config.js
+++ docusaurus-site/docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://git.xx.network/elixxir/xxdk-dev-docs',
             label: 'GitHub',
