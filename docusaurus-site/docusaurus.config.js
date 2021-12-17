59f39c724d5eca2fe2b0c4a5582d923e7ed8357a
jakub <jakub@elixxir.io>
2021-12-18 00:58:34 +0100

    More ci try

--- docusaurus-site/docusaurus.config.js
+++ docusaurus-site/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
             items: [
               {
                 label: 'Getting Started',
-                to: '/docs/getting-started',
+                to: 'getting-started',
               },
             ],
           }
