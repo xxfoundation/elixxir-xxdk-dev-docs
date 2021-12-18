071d66c7f04e90c271b2e2c266e239319a2ccdca
jakub <jakub@elixxir.io>
2021-12-18 01:00:16 +0100

    Fix CI

--- docusaurus-site/docusaurus.config.js
+++ docusaurus-site/docusaurus.config.js
@@ -60,17 +60,6 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Getting Started',
-                to: 'getting-started',
-              },
-            ],
-          }
-        ],
         copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
